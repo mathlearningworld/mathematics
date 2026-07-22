ac49df0679279ad82cccf0d2fb502083fa077cf0
mathlearningworld <mathlearningworld.project@gmail.com>
2026-07-22 22:13:20 +0700

    feat(world): export Builders Valley runtime ports

---
+++ src/builders-valley/runtime-ports/index.ts
@@ -0,0 +1,6 @@
+export type { CameraFocusRequest, CameraPort } from './camera.port';
+export type { TerrainAssemblyRequest, TerrainPort } from './terrain.port';
+export type { LandmarkProgressRecord, PersistencePort } from './persistence.port';
+export type { PlayerObservation, PlayerObservationPort } from './player-observation.port';
+export type { LightingIntentRequest, LightingPort } from './lighting.port';
+export type { MaterialReferenceRequest, MaterialPort } from './material.port';
