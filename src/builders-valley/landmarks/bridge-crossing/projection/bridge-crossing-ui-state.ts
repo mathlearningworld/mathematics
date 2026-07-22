4c1b8c8d7233fb944412be357920673a46d5aa80
mathlearningworld <mathlearningworld.project@gmail.com>
2026-07-22 22:36:04 +0700

    feat(world): add Bridge Crossing UI state

---
+++ src/builders-valley/landmarks/bridge-crossing/projection/bridge-crossing-ui-state.ts
@@ -0,0 +1,10 @@
+export interface BridgeCrossingUiState {
+  readonly showApproachMarker: boolean;
+  readonly showObservationMarker: boolean;
+  readonly showPreparationMarker: boolean;
+  readonly showBlockedPath: boolean;
+  readonly showCrossablePath: boolean;
+  readonly showDestinationMarker: boolean;
+  readonly showCompletion: boolean;
+  readonly highlightBridge: boolean;
+}
