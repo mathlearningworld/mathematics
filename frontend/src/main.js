bcb196a8ef6b9e0ff256905e484ab765572cc5cf
mathlearningworld <mathlearningworld.project@gmail.com>
2026-07-22 16:00:54 +0700

    feat(graphics): load HERO-002.1 terrain mass calibration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -44,6 +44,7 @@ import "./sandbox/scenes/BuildersValleySceneCompositionPatch.js";
 import "./sandbox/scenes/BuildersValleyEnvironmentArtistPassPatch.js";
 import "./sandbox/scenes/BuildersValleyHeroCompositionCalibrationPatch.js";
 import "./sandbox/scenes/BuildersValleyHeroTerrainMassPatch.js";
+import "./sandbox/scenes/BuildersValleyHeroTerrainMassCalibrationPatch.js";
 import "./sandbox/scenes/BuildersValleyEnvironmentReleaseCandidatePatch.js";
 import "./sandbox/scenes/BuildersValleyAssetDebugPatch.js";
 
